440a8c0b1bcc7f8eb6f00cfb681f95e0a95cac92
Iurii <iamyurkas@gmail.com>
2026-08-20 16:42:16 +0300

    fix: align auth modals with design

--- frontend/src/components/LogOutModal/LogOutModal.jsx
+++ frontend/src/components/LogOutModal/LogOutModal.jsx
@@ -1,10 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import Button from '../Button/Button';
 import { useAppDispatch } from '../../redux/hooks';
 import { logout } from '../../redux/auth/slice';
 import { closeModal } from '../../redux/modals/slice';
-import { persistor } from '../../redux/store';
 import css from './LogOutModal.module.css';
 
 const LogOutModal = () => {
@@ -13,21 +11,40 @@ const LogOutModal = () => {
 
   const onLogout = async () => {
     await dispatch(logout());
-    await persistor.purge();
-    localStorage.removeItem('token');
     dispatch(closeModal());
     navigate('/');
     toast.success('You have been logged out');
   };
 
+  const onCancel = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <div className={css.wrap}>
-      <h2 className={css.title}>Log out</h2>
-      <p className={css.text}>Are you sure you want to log out?</p>
-      <button className={css.cancel} type="button" onClick={() => dispatch(closeModal())}>
-        Cancel
-      </button>
-      <Button onClick={onLogout}>Log out</Button>
+      <h2 className={css.title}>Are you logging out?</h2>
+
+      <p className={css.text}>
+        You can always log back in at my time.
+      </p>
+
+      <div className={css.actions}>
+        <button
+          className={css.logout}
+          type="button"
+          onClick={onLogout}
+        >
+          Log out
+        </button>
+
+        <button
+          className={css.cancel}
+          type="button"
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };
